a6407de74418abca055a6d090eaba76a4054bf34
jwdavis1989 <jwdavis1989@gmail.com>
2021-02-03 18:01:54 -0600

    Postgres Integration Complete

--- Server/user-service/src/main/resources/data.sql
+++ Server/user-service/src/main/resources/data.sql
@@ -1,2 +1,14 @@
-INSERT INTO USER(USER_ID, DEPARTMENT_ID, EMAIL, FIRST_NAME, LAST_NAME)
-VALUES(1, 1, 'test1@test.com', 'Test', '1');
+INSERT INTO empschema."user"(USER_ID, DEPARTMENT_ID, EMAIL, FIRST_NAME, LAST_NAME)
+VALUES(1, 1, 'test1@test.com', 'Test', 'Case');
+
+INSERT INTO empschema."user"(USER_ID, DEPARTMENT_ID, EMAIL, FIRST_NAME, LAST_NAME)
+VALUES(2, 1, 'test1@test.com', 'Toby', 'Johnson');
+
+INSERT INTO empschema."user"(USER_ID, DEPARTMENT_ID, EMAIL, FIRST_NAME, LAST_NAME)
+VALUES(3, 1, 'test1@test.com', 'Frank', 'Davis');
+
+INSERT INTO empschema."user"(USER_ID, DEPARTMENT_ID, EMAIL, FIRST_NAME, LAST_NAME)
+VALUES(4, 1, 'test1@test.com', 'Jeremy', 'Boudreaux');
+
+INSERT INTO empschema."user"(USER_ID, DEPARTMENT_ID, EMAIL, FIRST_NAME, LAST_NAME)
+VALUES(5, 1, 'test1@test.com', 'Timothy', 'No One Cares');
