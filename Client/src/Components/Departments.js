8fc03428c91458a2a65e3bb74e91757723ae4536
Frank <frj071196@hotmail.com>
2021-02-04 15:05:22 -0600

    Added Deparment component

---
+++ Client/src/Components/Departments.js
@@ -0,0 +1,49 @@
+import {useState} from 'react';
+import axios from 'axios';
+import DepartmentData from '../Views/DepartmentData';
+
+const Departments = () => {
+
+    const getDepartmentUrl = 'http://localhost:9191/departments/';
+    const [state, setState] = useState("");
+    const catchString = "Please enter a valid Department ID.";
+
+    const getDepartment = async (id) => {
+        try {
+            await axios.get(getDepartmentUrl+id)
+            .then(resp => (setState(resp.data)));
+        } catch {
+            setState(catchString);
+        }
+        
+    }
+
+    return (
+        <div className="departmentContainer">
+            {console.log(state)}
+            <h1>Departments</h1>
+            <input type="text" placeholder="Enter an ID number" id="departmentId" onChange={e => (getDepartment(e.target.value))}/>
+            <table className="table table-dark table-striped" id="department-table">
+                <thead>
+                    <tr>
+                        <th>Department ID</th>
+                        <th>Department Name</th>
+                        <th>Department Address</th>
+                        <th>Department Code</th>
+                    </tr>
+                </thead>
+                <tbody id="department-table-data">
+                    {
+                        state !== "" && state !== undefined ? (
+                            <DepartmentData state={state}/>
+                        ) : (
+                            <tr>{catchString}</tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Departments;
